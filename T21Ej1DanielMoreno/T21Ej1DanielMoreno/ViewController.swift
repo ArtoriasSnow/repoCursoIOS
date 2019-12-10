25f7e30297ce598639bae24c0eb723de3d630218
Daniel Moreno <daniel.moreno@avantgardeit.es>
2019-12-10 22:14:22 +0100

    The first commit

---
+++ T21Ej1DanielMoreno/T21Ej1DanielMoreno/ViewController.swift
@@ -0,0 +1,23 @@
+//
+//  ViewController.swift
+//  T21Ej1DanielMoreno
+//
+//  Created by Avantgarde on 10/12/2019.
+//  Copyright © 2019 Imagina Group. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        
+        
+        //AWESOME CHANGE IN THE PROJECT!!
+    }
+
+
+}
+
---
+++ T21Ej1DanielMoreno/T21Ej1DanielMorenoTests/T21Ej1DanielMorenoTests.swift
@@ -0,0 +1,34 @@
+//
+//  T21Ej1DanielMorenoTests.swift
+//  T21Ej1DanielMorenoTests
+//
+//  Created by Avantgarde on 10/12/2019.
+//  Copyright © 2019 Imagina Group. All rights reserved.
+//
+
+import XCTest
+@testable import T21Ej1DanielMoreno
+
+class T21Ej1DanielMorenoTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
